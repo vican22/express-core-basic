8265c44418906392a2f1b25b4b1b96cc87229d5f
Vedran <vedranknezevic1@yahoo.com>
2019-08-18 14:26:36 +0200

    introducing express functionality

---
+++ app.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const adminRoutes = require("./routes/admin");
+const userRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  if (req.originalUrl && req.originalUrl.split("/").pop() === "favicon.ico") {
+    return res.sendStatus(204);
+  }
+
+  return next();
+});
+
+app.use("/admin", adminRoutes);
+app.use(userRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
+
+app.listen(3000);
